e8159900f485ef43e265eea54592fd71841726ba
Sophie <bersenjowa@gmail.com>
2025-02-20 11:37:04 +0500

    update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,35 @@
 import "./Header.css";
-import { Search, Position, Xmark, NavArrowRight } from "iconoir-react";
+import { Search, Position, Xmark, NavArrowRight, User } from "iconoir-react";
 
 export function Header() {
     return (
         <div className="header">
+            <img src="/logo-green-fill.svg" alt="logo" className="logo" />
             <form action="" className="input__search">
-            <Search className="icons"/>
-            <input type="text" className="search"
-            placeholder="Искать в Гастрофаст"
-            />
-            <button>
-            <Xmark />
-            </button>
+                <Search className="icons" />
+                <input type="text" className="search"
+                    placeholder="Искать в Гастрофаст"
+                />
+                <button className="cancel__search" name="cancel search">
+                    <Xmark className="icons" />
+                </button>
             </form>
 
             <form action="" className="input__adress">
-            <Position className="icons"/>
-            <input type="text" className="adress"
-            placeholder="Лесная, 14Б"
-            />
-            <button>
-            <NavArrowRight />
-            </button>
+                <Position className="icons" />
+                <span className="adress__is">Адрес</span>
+                <input type="text" className="adress"
+                    placeholder="Лесная, 14Б"
+                    readOnly
+                />
+                <button className="change__location" name="change location">
+                    <NavArrowRight className="icons" />
+                </button>
             </form>
-            </div>
+            <button className="sign__in" name="sign in">
+                <User className="icons"/>
+                Войти
+            </button>
+        </div>
     );
 };
